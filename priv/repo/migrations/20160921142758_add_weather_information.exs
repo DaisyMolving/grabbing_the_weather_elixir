5a1bb704ab88f2148dd7e4b8d7571371e34c273c
Daisy Molving <daisymolving@gmail.com>
2016-09-21 16:02:09 +0100

    modified migration that temperature field is a decimal instead of an integer, created model file for weather_information

--- priv/repo/migrations/20160921142758_add_weather_information.exs
+++ priv/repo/migrations/20160921142758_add_weather_information.exs
@@ -4,7 +4,7 @@ defmodule GrabbingTheWeather.Repo.Migrations.AddWeatherInformation do
   def change do
     create table(:weather_information) do
       add :city, :string
-      add :temperature, :integer
+      add :temperature, :decimal
       add :description, :string
 
       timestamps
