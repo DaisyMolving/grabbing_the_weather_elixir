bad7bb8be2c45dee589d1db7198ed1ad975e6e5c
Daisy Molving <daisymolving@gmail.com>
2016-09-23 11:00:52 +0100

    removes unused configurations from config file

--- config/config.exs
+++ config/config.exs
@@ -9,22 +9,6 @@ config :grabbing_the_weather, GrabbingTheWeather.Repo,
   password: "postgres",
   hostname: "localhost"
 
-
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# 3rd-party users, it should be done in your "mix.exs" file.
-
-# You can configure for your application as:
-#
-    config :grabbing_the_weather, 
-    ecto_repos: [GrabbingTheWeather.Repo],
-    adapter: Ecto.Adapters.Postgres,
-    database: "weather",
-    username: "postgres",
-    password: "postgres"
-
 # And access this configuration in your application as:
 #
 #     Application.get_env(:grabbing_the_weather, :key)
@@ -32,7 +16,6 @@ config :grabbing_the_weather, GrabbingTheWeather.Repo,
 # Or configure a 3rd-party app:
 #
 #     config :logger, level: :info
-#
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
